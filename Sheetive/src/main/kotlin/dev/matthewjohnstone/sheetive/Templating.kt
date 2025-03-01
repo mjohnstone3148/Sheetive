691321e1b5c0b0e10843c53acc9a461a8ed811f1
Matthew Johnstone <mjohnstone357@gmail.com>
2025-03-01 12:20:59 +0000

    feat: Allow user to increment the counter

--- Sheetive/src/main/kotlin/dev/matthewjohnstone/sheetive/Templating.kt
+++ Sheetive/src/main/kotlin/dev/matthewjohnstone/sheetive/Templating.kt
@@ -1,8 +1,11 @@
 package dev.matthewjohnstone.sheetive
 
+import arrow.fx.stm.STM
 import arrow.fx.stm.TVar
+import arrow.fx.stm.atomically
 import dev.matthewjohnstone.sheetive.model.System
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.Thymeleaf
@@ -26,6 +29,22 @@ fun Application.configureTemplating(systemVar: TVar<System>) {
             val system = systemVar.unsafeRead()
             call.respond(ThymeleafContent("system", mapOf("system" to system)))
         }
+
+        post("/system") {
+            val formContent = call.receiveParameters()
+            val amountToIncrementParameter = formContent["amountToIncrement"] ?: "1"
+            val amountToIncrement = amountToIncrementParameter.toInt()
+            atomically { incrementCounter(systemVar, amountToIncrement) }
+            val system = systemVar.unsafeRead()
+            call.respond(ThymeleafContent("system", mapOf("system" to system)))
+        }
     }
 }
+
+fun STM.incrementCounter(systemVar: TVar<System>, amountToIncrement: Int) {
+    val oldSystem = systemVar.read()
+    val newSystem = oldSystem.copy(counter = oldSystem.counter + amountToIncrement)
+    systemVar.write(newSystem)
+}
+
 data class ThymeleafUser(val id: Int, val name: String)
