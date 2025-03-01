4ae61eeb1476914db49d959c260236c709251428
Matthew Johnstone <mjohnstone357@gmail.com>
2025-03-01 11:00:24 +0000

    feat: Initial Ktor project template

---
+++ Sheetive/src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package dev.matthewjohnstone
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureTemplating()
+    configureRouting()
+}
